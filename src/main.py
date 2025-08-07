c18870737e3a0a334d11178ab97d53189000f0f7
txrunn <tarun.gunaseelan@gmail.com>
2025-08-07 09:14:35 -0600

    update imports

--- src/main.py
+++ src/main.py
@@ -5,11 +5,11 @@ You will run this problem set from main.py, so set things up accordingly
 import os
 import numpy as np
 import pandas as pd
-import etl
-import preprocessing
-import logistic_regression
-import decision_tree
-import calibration_plot
+import part1_etl as etl
+import part2_preprocessing as preprocessing
+import part3_logistic_regression as logistic_regression
+import part4_decision_tree as decision_tree
+import part5_calibration_plot as calibration_plot
 
 
 # Call functions / instanciate objects from the .py files
